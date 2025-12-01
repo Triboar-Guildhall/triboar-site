cf245badd9a6601914345b135d2629c525164597
Adam Stewart <adam@gizra.com>
2025-12-01 11:48:52 -0600

    refactor.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,16 @@ module.exports = {
         'guild-beige-light': '#E8DCC6',
         'guild-beige-dark': '#C0B094',
         'guild-gold': '#B8860B',
+        'guild-gold-dark': '#d4a94c',
         'guild-blue': '#2C3E50',
         'guild-blue-dark': '#1A252F',
         'guild-blue-light': '#34495E',
+        'guild-blue-darker': '#2f3a4f',
+        'parchment-border': '#8b6f47',
+      },
+      fontFamily: {
+        'serif': ['Merriweather', 'Georgia', 'serif'],
+        'sans': ['Inter', 'Arial', 'sans-serif'],
       }
     },
   },
